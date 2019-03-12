b1c8efa75bc99b83d406c52e198672f581a82875
ikraduya <ikraduya@gmail.com>
2019-03-12 12:00:02 +0700

    add linked list

---
+++ LinkedList.h
@@ -0,0 +1,48 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+/**
+ * Kelas LinkedList yang mampu menyimpan tipe generic
+ */
+
+template <class T>
+class LinkedList {
+private:
+  struct Node* head = NULL; /**< head of linkedlist */
+
+public:
+  /**
+   * Node untuk menyimpan tiap elemen
+   */
+  struct Node {
+    T data;             /**< struct data */
+    struct Node *next;  /**< struct next pointer */
+  }
+
+  /**
+   * Mengembalikan indeks dimana elemen ditemukan, -1 jika tidak ada
+   */
+  int find(T el) const;
+
+  /**
+   * Mengembalikan True jika linked list kosong
+   */
+  bool isEmpty() const;
+
+  /**
+   * Menambahkan elemen sebagai elemen paling akhir
+   */
+  void add(T el);
+
+  /**
+   * Membuang elemen dari linked list
+   */
+  void remove(T el);
+
+  /**
+   * Mengembalikan elemen pada indeks
+   */
+  T get(int idx) const;
+};
+
+#endif
