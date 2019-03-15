79b912998588268c3aff4c4926293ee7580fbd87
unknown <akhmaliswara@gmail.com>
2019-03-15 16:37:07 +0700

    edit comment Player.h

--- Player.h
+++ Player.h
@@ -1,69 +1,194 @@
-// File : Player.h
-// Modul ini berisi kode untuk mengatur player (move, interact, talk)
-
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "EngisFarm.h"
 
-enum arahEnum {UP, DOWN, LEFT, RIGT};
+/**
+* Kelas Player untuk segala aksi dan atribut yang dimiliki player
+*/
 
+enum arahEnum {
+	UP,  /*!< Enum value Up. */  
+	DOWN,  /*!< Enum value Down. */ 
+	LEFT,  /*!< Enum value Left. */ 
+	RIGHT /*!< Enum value Right. */ 
+};
+
+/**
+* Konstanta max inventory
+*/
 const int MaxInventory = 20;
+/**
+* Konstanta max water
+*/
 const int MaxWater = 20;
 
 class Player {
 	private:
-		int jumlahInventori; // Jumlah Barang saat ini
-		LinkedList<Product> inventori; // Array Inventori
-		int wadahAir; // Boolean wadah air terisi atau belum
-		int uang; // Uang yang dimiliki
-		arahEnum arah; // Arah player menghadap
-		int absis; // Position X
-		int ordinat; // Position Y
+		/**
+		* Jumlah Barang saat ini
+		*/
+		int jumlahInventori; 
+
+		/**
+		* Array Inventori
+		*/
+		LinkedList<Product> inventori; 
+
+		/**
+		* Wadah air yang sudah terisi
+		*/
+		int wadahAir; 
+
+		/**
+		* Uang yang dimiliki
+		*/
+		int uang; 
+
+		/**
+		* Arah player menghadap
+		*/
+		arahEnum arah; 
+
+		/**
+		* Position X
+		*/
+		int absis; 
+
+		/**
+		* Position Y
+		*/
+		int ordinat; 
+
 	public:
-		// default constructor
+		/**
+		* default constructor
+		*/
 		Player();
 
-		// destructor
+		/**
+		* destructor
+		*/
 		~Player();
 
-		// Getter dan setter
-		void setJumlahInventori(int); // Set jumlah inventori
-		int getJumlahInventori();  // Get jumlah inventori
-		void setWadahAir(int); // Set jumlah air
-		int getWadahAir(); // Get jumlah air
-		void setUang(int); // Set nilai uang
-		int getUang(); // Get nilai uang
-		void setArah(arahEnum); // Set arah player menghadap
-		arahEnum getArah(); // Get arah player menghadap
-		void setAbsis(int); // Set posisi absis player
-		int getAbsis(); // Get posisi absis player
-		void setOrdinat(int); // Set posisi ordinat player
-		int setOrdinat(); // Get posisi ordinat player
-
+		/**
+		* Getter dan setter
+		* Set jumlah inventori
+		*/
+		void setJumlahInventori(int); 
+		/**
+		* Get jumlah inventori
+		*/
+		int getJumlahInventori();  
+		/**
+		* Set jumlah air
+		*/
+		void setWadahAir(int); 
+		/**
+		* Get jumlah air
+		*/
+		int getWadahAir(); 
+		/**
+		* Set nilai uang
+		*/
+		void setUang(int); 
+		/**
+		* Get nilai uang
+		*/
+		int getUang(); 
+		/**
+		* Set arah player menghadap
+		*/
+		void setArah(arahEnum); 
+		/**
+		* Get arah player menghadap
+		*/
+		arahEnum getArah(); 
+		/**
+		* Set posisi absis player
+		*/
+		void setAbsis(int); 
+		/**
+		* Get posisi absis player
+		*/
+		int getAbsis(); 
+		/**
+		* Set posisi ordinat player
+		*/
+		void setOrdinat(int); 
+		/**
+		* Get posisi ordinat player
+		*/
+		int setOrdinat(); 
 		
-		// Player bergerak
-		void up(); // Player pindah ke atas
-		void down(); // Player pindah ke bawah
-		void left(); // Player pindah ke kiri
-		void right(); // Player pindah ke kanan
+		/**
+		* Player bergerak
+		* Player pindah ke atas
+		*/
+		void up(); 
+		/**
+		* Player pindah ke bawah
+		*/
+		void down(); 
+		/**
+		* Player pindah ke kiri
+		*/
+		void left(); 
+		/**
+		* Player pindah ke kanan
+		*/
+		void right();
 
-		void lookUp(); // Player menghadap ke atas
-		void lookDown(); // Player menghadap ke bawah
-		void lookLeft(); // Player menghadap ke kiri
-		void lookRight(); // Player menghadap ke kanan
+		/**
+		* Player menghadap ke atas
+		*/
+		void lookUp(); 
+		/**
+		* Player menghadap ke bawah
+		*/
+		void lookDown(); 
+		/**
+		* Player menghadap ke kiri
+		*/
+		void lookLeft(); 
+		/**
+		* Player menghadap ke kanan
+		*/
+		void lookRight(); 
 
-		// Command dengan animal
+		/**
+		* Command dengan animal
+		* Berbicara dengan hewan
+		*/
 		void talk();
+		/**
+		* Berinteraksi dengan Farm Animal
+		*/
 		void interact(FarmAnimal &x);
-		
-		//void kill();
-		//void grow(); 
-
-		// Command dengan facility
-		void cekInventory(); // Melihat inventori
-		void fillWater(); // Isi air
-		void truck(); // Mengosongkan bag dan jual {
-		void mixProduct(); // Mix dengan mixer
+		/**
+		* User memberi perintah kill
+		*/
+		void cmdKill();
+		/**
+		* User memberi perintah grow
+		*/
+		void cmdGrow(); 
+		/**
+		* Command dengan facility
+		* Melihat inventori
+		*/
+		void cekInventory(); 
+		/**
+		* Isi air
+		*/
+		void fillWater(); 
+		/**
+		* Mengosongkan bag dan jual 
+		*/
+		void truck();
+		/**
+		* Mix dengan mixer
+		*/
+		void mixProduct(); 
 };
 
 #endif
