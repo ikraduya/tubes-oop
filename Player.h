6768511466f2a2e908c89a77ad7f04fff3a8c554
unknown <akhmaliswara@gmail.com>
2019-03-15 08:24:22 +0700

    update Player.h

--- Player.h
+++ Player.h
@@ -1,19 +1,24 @@
+// File : Player.h
+// Modul ini berisi kode untuk mengatur player (move, interact, talk)
+
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "EngisFarm.h"
-#define MaxInventory 20
 
 enum arahEnum {UP, DOWN, LEFT, RIGT};
 
-const int MaxWater 20
+const int MaxInventory = 20;
+const int MaxWater = 20;
 
 class Player {
 	private:
-		int JumlahInventori; //Jumlah Barang saat ini
-		LinkedList<Product> Inventori; //Array Inventori
-		int WadahAir; //Boolean wadah air terisi atau belum
-		int Uang;
-		arahEnum arah;
+		int jumlahInventori; // Jumlah Barang saat ini
+		LinkedList<Product> inventori; // Array Inventori
+		int wadahAir; // Boolean wadah air terisi atau belum
+		int uang; // Uang yang dimiliki
+		arahEnum arah; // Arah player menghadap
+		int absis; // Position X
+		int ordinat; // Position Y
 	public:
 		// default constructor
 		Player();
@@ -22,35 +27,43 @@ class Player {
 		~Player();
 
 		// Getter dan setter
-		void setJumlahInventori();
-		int getJumlahInventori();
-		void setWadahAir();
-		bool getWadahAir();
+		void setJumlahInventori(int); // Set jumlah inventori
+		int getJumlahInventori();  // Get jumlah inventori
+		void setWadahAir(int); // Set jumlah air
+		int getWadahAir(); // Get jumlah air
+		void setUang(int); // Set nilai uang
+		int getUang(); // Get nilai uang
+		void setArah(arahEnum);
+		arahEnum getArah();
+		void setAbsis(int); // Set posisi absis player
+		int getAbsis(); // Get posisi absis player
+		void setOrdinat(int); // Set posisi ordinat player
+		int setOrdinat(); // Get posisi ordinat player
+
 		
 		// Player bergerak
-		void up();
-		void down();
-		void left();
-		void right();
+		void up(); // Player pindah ke atas
+		void down(); // Player pindah ke bawah
+		void left(); // Player pindah ke kiri
+		void right(); // Player pindah ke kanan
 
-		void lookUp();
-		void lookDown();
-		void lookLeft();
-		void lookRight();
+		void lookUp(); // Player menghadap ke atas
+		void lookDown(); // Player menghadap ke bawah
+		void lookLeft(); // Player menghadap ke kiri
+		void lookRight(); // Player menghadap ke kanan
 
 		// Command dengan animal
-		void Talk();
-		/* Kayanya ini ntar bisa pake generic / fungsi template */
-		void Interact(FarmAnimal &x);
+		void talk();
+		void interact(FarmAnimal &x);
 		
-		void Kill();
-		void Grow();
+		//void kill();
+		//void grow(); 
 
 		// Command dengan facility
-		void CekInventory(); 
-		void FillWater(); // Isi air
-		void Truck(); // Mengosongkan bag dan jual {
-		void MixProduct(); // Mix dengan mixer
+		void cekInventory(); // Melihat inventori
+		void fillWater(); // Isi air
+		void truck(); // Mengosongkan bag dan jual {
+		void mixProduct(); // Mix dengan mixer
 };
 
 #endif
